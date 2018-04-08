d20cce2e9dacc1b92ef2026ba0af06c88bf29d06
tika <a>
2018-04-08 22:52:06 +0900

    DrawerにDrawFomatStrng機能を追加

--- Drawer.h
+++ Drawer.h
@@ -28,7 +28,8 @@ public:
 	void drawBox( float x1, float y1, float x2, float y2, int color, bool fillflag );
 	void drawCircle( float x, float y, float r, int color, bool fillflag );
 	void drawLine( float x1, float y1, float x2, float y2, int color );
-	void drawString( float x, float y, int color, std::string str );
+	void drawString( float x, float y, std::string str, unsigned int color );
+	void drawFormatString( float x, float y, unsigned int color, const char *str, ... );
 	void flip( );
 
 public:
